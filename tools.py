235b117ac18dadd5178286b89b6af6aa9fddf26e
marianami19 <45075042+marianami19@users.noreply.github.com>
2026-08-24 20:30:01 -0400

    ERROR

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 import ast
 import re
-from langchain_core.tools.retriever import create_retriever_tool
+from langchain.tools.retriever import create_retriever_tool
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
